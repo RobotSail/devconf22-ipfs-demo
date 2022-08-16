f7e1405ca5a53a89ec312365ac5e8d3ebf4b5043
Oleg <97077423+RobotSail@users.noreply.github.com>
2022-08-16 18:39:43 -0400

    add ipfs target
    Signed-off-by: Oleg <97077423+RobotSail@users.noreply.github.com>

--- ipfs-demo/src/App.svelte
+++ ipfs-demo/src/App.svelte
@@ -1,12 +1,13 @@
 <script>
   import svelteLogo from './assets/svelte.svg'
   import Counter from './lib/Counter.svelte'
+	import viteLogo from './assets/vite.svg'
 </script>
 
 <main>
   <div>
     <a href="https://vitejs.dev" target="_blank"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
+      <img src={viteLogo} class="logo" alt="Vite Logo" />
     </a>
     <a href="https://svelte.dev" target="_blank"> 
       <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
